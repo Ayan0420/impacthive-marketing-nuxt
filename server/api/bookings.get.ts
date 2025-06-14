e1453784816ba8df199c1190533f1e98ebb26044
Jerry Clark <jerry.cabuntucan@tuitt.com>
2025-06-14 23:08:01 +0800

    removed unwanted files

--- server/api/bookings.get.ts
+++
@@ -1,8 +0,0 @@
-import { connectToDB } from '~/server/utils/mongoose'
-import { Booking } from '~/server/models/Booking'
-
-export default defineEventHandler(async () => {
-  await connectToDB()
-  const bookings = await Booking.find().sort({ date: 1 })
-  return bookings
-})
